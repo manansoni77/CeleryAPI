91310ee83d0da3ea833283a712ecf33864390987
MananSoniLinux <manansoni.soni77@gmail.com>
2023-10-16 00:44:35 +0530

    Gmail email (working)

---
+++ activity_tracker_v2/gmail.py
@@ -0,0 +1,47 @@
+import httplib2
+import base64
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from apiclient import errors, discovery
+# import mimetypes
+# from email.mime.image import MIMEImage
+# from email.mime.audio import MIMEAudio
+# from email.mime.base import MIMEBase
+from worker import app
+from email_cred import get_credentials
+
+@app.task(name='sendGmail')
+def SendMessage(sender, to, subject, msgHtml, msgPlain):
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('gmail', 'v1', http=http)
+    message1 = CreateMessage(sender, to, subject, msgHtml, msgPlain)
+    SendMessageInternal(service, "me", message1)
+
+def SendMessageInternal(service, user_id, message):
+    try:
+        message = (service.users().messages().send(userId=user_id, body=message).execute())
+        print('Message Id: %s' % message['id'])
+        return message
+    except errors.HttpError as error:
+        print('An error occurred: %s' % error)
+
+def CreateMessage(sender, to, subject, msgHtml, msgPlain):
+    msg = MIMEMultipart('alternative')
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = to
+    msg.attach(MIMEText(msgPlain, 'plain'))
+    msg.attach(MIMEText(msgHtml, 'html'))
+    raw = base64.urlsafe_b64encode(msg.as_bytes())
+    raw = raw.decode()
+    body = {'raw': raw}
+    return body
+
+to = "manansoni.soni77@gmail.com"
+sender = "mananapps7@gmail.com"
+subject = "subject"
+msgHtml = "Hi<br/>Html Email"
+msgPlain = "Hi\nPlain Email"
+
+SendMessage(sender, to, subject, msgHtml, msgPlain)
